50731ec29161e0c5cd14ed8876aee5e911f66028
dis-zephyro <zephyro@yandex.ru>
2015-11-30 02:36:32 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -181,3 +181,30 @@ $(".island-more").fancybox({
         $('.island').popover('hide');
     }
 });
+
+
+$('[data-type="slider-scroller"]').mousemove((function () {
+    var
+        $scroller = $('[data-type="slider-scroller"]'),
+        $img = $('[data-type="slider-image"]'),
+        wrapperHeight = parseInt($('[data-type="slider-image-wrapper"]').height()),
+        imgHeight = parseInt($img.height()),
+        step = (imgHeight - wrapperHeight) / 100;
+
+
+    return function () {
+        //noinspection JSJQueryEfficiency
+        var val, top;
+
+        imgHeight = parseInt($img.height());
+        step = (imgHeight - wrapperHeight) / 100;
+
+        val = $scroller.val();
+        top = val * step;
+        top = '-' + top + 'px';
+
+        $img.css('top', top);
+
+        console.log('top', top);
+    };
+})());
